e27aa4943f744ffa0fbf753a616f8fbc2a739978
sagedemage <salsaeed999@gmail.com>
2021-12-06 18:25:12 -0500

    initial commit

---
+++ app/app.py
@@ -0,0 +1,46 @@
+"""Web Application Program"""
+from flask import Flask, render_template, request, flash
+from calc.calculator import Calculator
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def homepage():
+    """Display homepage"""
+    return render_template('index.html')
+
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    """Display the form"""
+    if request.method == 'POST':
+        # name of the tag
+        value1 = request.form['value1']
+        value2 = request.form['value2']
+        operation = request.form['operation']
+        getattr(Calculator, operation)(value1, (value2,))
+        result = ""
+        if Calculator.get_result_of_last_calculation_in_history() is None:
+            flash("Error, can't divide by zero", "error")
+            result = "None"
+        else:
+            result = str(Calculator.get_result_of_last_calculation_in_history())
+            flash("Success!", "success")
+        return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+    else:
+        return render_template('form.html')
+
+
+@app.route('/result')
+def result_message():
+    """Display the result"""
+    value1 = request.args.get('value1')
+    value2 = request.args.get('value2')
+    operation = request.args.get('operation')
+    result = request.args.get('result') # something is going please fix this
+    message = operation + " of " + value1 + " " + value2 + " is " + result
+    flash('You were successfully logged in')
+    return message
+    # return render_template('result.html')
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Flash Messages and Validation
+
+**The web application of the calculator program shows success 
+and error flash messages of the calculation. The web application 
+uses flask and jinga**
+
+## Flash Messages Output
+![](images/error_flash_message.png)
+![](images/success_flash_message.png)
